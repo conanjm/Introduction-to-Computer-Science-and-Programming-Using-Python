65652747e35098802062a9f2df02e2b0cabd341e
tasoskalogero <tasoskalogero@gmail.com>
2015-01-23 17:10:43 +0200

    Create lenIter.py

---
+++ lenIter.py
@@ -0,0 +1,11 @@
+def lenIter(aStr):
+    '''
+    aStr: a string
+
+    returns: int, the length of aStr
+    '''
+    # Your code here
+    count = 0;
+    for s in aStr:
+        count += 1
+    return count
