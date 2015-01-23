f27413fab861a3bb1f1d548c10a74e0a3d5dcec2
tasoskalogero <tasoskalogero@gmail.com>
2015-01-23 15:30:56 +0200

    Create iterPower.py

---
+++ iterPower.py
@@ -0,0 +1,14 @@
+def iterPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+ 
+    returns: int or float, base^exp
+    '''
+    # Your code here
+    if exp == 0:
+        return 1
+    result = base
+    for i in range(0,exp-1):
+        result *= base
+    return result
